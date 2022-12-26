0d2bf5feb6088ea616f718f8dfab0c290d3e2910
Mrityunjay Kumar <kumarmrityunjay7034@gmail.com>
2022-12-26 21:17:52 +0530

    logging data

--- handlers/requestHandler.js
+++ handlers/requestHandler.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const requestHandler = async (req) => {
   try {
 
-    console.log("Body\n\n".repeat(10), req.body);
+    console.log("Incoming line webhook data...\n".repeat(10), JSON.stringify(req.body, null, 4));
 
     const res = await axios({
       url: "https://api.line.me/v2/bot/message/reply",
